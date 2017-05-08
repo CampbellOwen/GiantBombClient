c0178457a3b6b7730597615dd0d5480b9c86f77c
Owen Campbell <ocampbel@sfu.ca>
2017-05-08 12:21:12 -0700

    Added loginmanager

---
+++ GiantBombClient/Utilities/LoginManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiantBombClient.Utilities
+{
+    public sealed class LoginManager
+    {
+        #region Singleton Stuff
+
+        private static readonly Lazy<LoginManager> lazy = new Lazy<LoginManager>(() => new LoginManager());
+
+        public static LoginManager Instance { get { return lazy.Value; } }
+
+        private LoginManager()
+        {
+
+        }
+
+        #endregion
+
+        #region Fields
+
+        #endregion
+
+        #region Properties
+
+        public string ApiKey { get; private set; }
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
