a921bee4b086908a624df9e26533d33e5e279e89
Owen Campbell <ocampbel@sfu.ca>
2017-05-10 15:28:35 -0700

    Added logging to network provider

--- GiantBombClient/Utilities/NetworkProviders/DefaultNetworkProvider.cs
+++ GiantBombClient/Utilities/NetworkProviders/DefaultNetworkProvider.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
@@ -21,8 +22,19 @@ namespace GiantBombClient.Utilities.NetworkProviders
 
         public async Task<string> GetAsync(Uri uri)
         {
+            Debug.WriteLine(string.Format("[DEBUG] Making GET request to {0}", uri.ToString()));
             var httpResponse = await httpClient.GetAsync(uri);
-            httpResponse.EnsureSuccessStatusCode();
+            try
+            {
+                httpResponse.EnsureSuccessStatusCode();
+            }
+            catch (Exception)
+            {
+                Debug.WriteLine("[DEBUG] Request returned bad status code");
+                return "";
+            }
+            Debug.WriteLine("[DEBUG] Request successful");
+
             return await httpResponse.Content.ReadAsStringAsync();
         }
     }
